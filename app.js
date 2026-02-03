067c5faf0af6429f7088139468f10c93ea09bb02
Yash <yash170705@gmail.com>
2026-02-03 21:30:31 +0530

    CRUD  operations set up

--- app.js
+++ app.js
@@ -1,21 +1,78 @@
-import express from "express"
-import mongoose from "mongoose"
-const app=express();
-const MONGO_URL="mongodb://127.0.0.1:27017/compass"
-async function main() {
-        await mongoose.connect(MONGO_URL)
-}
-main().then(()=>{
-    console.log("cinnected to db")
-}).catch(err=>{
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride=require("method-override")
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/compass";
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
 
 
-app.listen(8080,()=>{
-    console.log("server listening at 8080");
-});
-app.get((req,res)=>{
-    res.send("i am route");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
+//Index Route
+app.get("/listings", async (req, res) => {
+  const allListings= await Listing.find({})
+  res.render("listings/index.ejs",{allListings});
+});
+//new route
+app.get("/listings/new",(req,res)=>{
+  res.render("listings/new.ejs");
+})
+//show route
+app.get("/listings/:id",async(req,res)=>{
+  let {id}=req.params
+  const listing=await Listing.findById(id)
+  res.render("listings/show.ejs",{listing})
+})
+
+//create route
+app.post("/listings",async(req,res)=>{
+  // let {title,description,image,price,country,location}=req.body;
+  const newlisting=new Listing(req.body.listing);
+  console.log(newlisting);
+  res.redirect("/listings")
+  
+})
+
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+  let {id}=req.params
+  const listing=await Listing.findById(id)
+  res.render("listings/edit.ejs",{listing})
+})
+
+//update route
+app.put("/listings/:id",async(req,res)=>{
+  let {id}=req.params
+  await Listing.findByIdAndUpdate(id,{...req.body.listing})
+  res.redirect(`/listings/${id}`)
+})
+//delete route
+app.delete("/listings/:id",async(req,res)=>{
+  let {id}=req.params
+  let deletedListing= await Listing.findByIdAndDelete(id)
+  console.log("delelte listing")
+  res.redirect("/listings")
+
+})
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?auto=format&fit=crop&w=800&q=60",
+    },
+    filename: {
+      type: String,
+      default: "defaultimage",
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+module.exports = mongoose.model("Listing", listingSchema);
