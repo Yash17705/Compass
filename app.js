4c5aa7a229c71795d58d0989454dfdd6ccc1abc6
Yash <yash170705@gmail.com>
2026-03-01 15:36:42 +0530

    more styling added to show page

--- app.js
+++ app.js
@@ -22,13 +22,12 @@ async function main() {
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.engine('ejs',ejsMate);
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
-app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
-
 });
 
 //Index Route
