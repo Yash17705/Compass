fca7b8287ddba4e093c091e4ce4a3b24564e1b09
Yash <yash170705@gmail.com>
2026-03-21 16:08:36 +0530

    added feature for deletion of reviews from db

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync=require("./uitls/wrapAsync.js");
 const ExpressError=require("./uitls/ExpressErrors.js")
-const {listingSchema}=require("./schema.js")
+const {listingSchema, reviewSchema}=require("./schema.js")
 const Review= require("./models/review.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/compass";
@@ -47,6 +47,17 @@ const validateListing=(req,res,next)=>{
   }
 }
 
+const validateReview=(req,res,next)=>{
+  let error = reviewSchema.validate(req.body);
+
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errMsg);
+  }else{
+    next();
+  }
+}
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
   const allListings= await Listing.find({})
@@ -59,7 +70,7 @@ app.get("/listings/new",(req,res)=>{
 //show route
 app.get("/listings/:id",wrapAsync(async(req,res)=>{
   let {id}=req.params
-  const listing=await Listing.findById(id)
+  const listing=await Listing.findById(id).populate("reviews")
   res.render("listings/show.ejs",{listing})
 }))
 
@@ -111,19 +122,29 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
 }))
 //Reviews
 //Post Route
-app.post("/listings/:id/reviews",async(req,res)=>{
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
   let listing= await Listing.findById(req.params.id)
-  let newReview= new Review(req.body.review)
+  let newReview= new Review({
+    ...req.body.review,
+    comment: req.body.review.comment.trim(),
+  });
   listing.reviews.push(newReview);
   await newReview.save();
   await listing.save();
   
   res.redirect(`/listings/${listing._id}`);
 
-})
+}))
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+  let{id,reviewId}=req.params;
+  await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/listings/${id}`);
+}))
 app.all("/{*splat}",(req,res,next)=>{
   next(new ExpressError(404,"Page not found"))
 })
+
 app.use((err,req,res,next)=>{
   if (res.headersSent) {
     return next(err);
