66eca2c5b1c6517063ca9dd24d8c242888e27b37
Yash <yash170705@gmail.com>
2026-03-24 23:24:29 +0530

    feat: added authentication using package passport

--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router= express.Router({mergeParams:true})
 const wrapAsync=require("../utils/wrapAsync.js");
+const { reviewSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressErrors.js");
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
 
 const validateReview=(req,res,next)=>{
-  let error = reviewSchema.validate(req.body);
+  let { error } = reviewSchema.validate(req.body);
 
   if(error){
     let errMsg=error.details.map((el)=>el.message).join(",");
